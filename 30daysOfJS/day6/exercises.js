2399d2614ac9dc2ea8af7d7f8b019594dac8d0a9
Viet Hung Dang <viet.dang@student.manchester.ac.uk>
2023-02-06 10:35:40 +0000

    Day 6 exercises level 2 finished

--- 30daysOfJS/day6/exercises.js
+++ 30daysOfJS/day6/exercises.js
@@ -1,3 +1,29 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  
+  const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
 //level 1
 // 1
 let arrayOfNumbers = []
@@ -191,3 +217,175 @@ for(let i = 0; i < 6; i++)
     }
 }
 console.log(randomId)
+
+
+//level 2
+//1 generate any number of characters random id
+const letRandomIdLength = 10
+smallAlphaCharMin = 97 //up to 122
+numberCharMin = 48 //up to 57
+randomId = ""
+for(let i = 0; i < letRandomIdLength; i++)
+{
+    let chooseCharOrNum = parseInt(Math.random()* (max - min) + min)
+    if(chooseCharOrNum % 2 == 0)
+    {
+        //choose a num
+        randomId = randomId.concat(String.fromCharCode(Math.random()* 10 + numberCharMin))
+    }
+    else
+    {
+        //choose a char
+        randomId = randomId.concat(String.fromCharCode(Math.random()* 26 + smallAlphaCharMin))
+    }
+}
+console.log(randomId)
+
+//2 random hex number
+let randomHexNumber = "#"
+const randomHexNumberLength = 6
+for(let i = 0; i < randomHexNumberLength; i++)
+{
+    let randomNum = parseInt(Math.random()* 16)
+    if( randomNum > 9 )
+    {
+        randomHexNumber = randomHexNumber.concat(String.fromCharCode(randomNum + smallAlphaCharMin - 9))
+    }
+    else
+    {
+        randomHexNumber = randomHexNumber.concat(randomNum.toString())
+    } 
+}
+console.log(randomHexNumber)
+
+//3 random RGB colour
+let rgbArray = [0,0,0]
+for(let i = 0; i < rgbArray.length; i++)
+{
+    rgbArray[i] = parseInt(Math.random()* 255)
+}
+console.log(`rgb(${rgbArray[0]},${rgbArray[1]},${rgbArray[2]})`)
+
+//4, 5
+let capitalisedCountries = []
+let countriesLengths = []
+for(let country of countries)
+{
+    capitalisedCountries.push(country.toUpperCase())
+    countriesLengths.push(country.length)
+}
+console.log(capitalisedCountries)
+console.log(countriesLengths)
+
+//6
+let countriesInfo = []
+for(let i = 0; i < countries.length; i++)
+{
+    countriesInfo.push([countries[i], capitalisedCountries[i].slice(0,3), countriesLengths[i] ])
+}
+console.log(countriesInfo)
+
+//7 check if any country contains "land"
+let landCountries = []
+for(let country of countries)
+{
+    if(country.includes("land"))
+    {
+        landCountries.push(country)
+    }
+}
+console.log(landCountries.length > 0 ? landCountries : 'All these countries are without land')
+
+//8
+let countriesEndingWithIa = []
+for(let country of countries)
+{
+    let lastIndexPosition = country.lastIndexOf("ia")
+    if( lastIndexPosition != -1 && country.length - "ia".length == lastIndexPosition)
+    {
+        countriesEndingWithIa.push(country)
+    }
+}
+console.log(countriesEndingWithIa.length > 0 ? countriesEndingWithIa : 'These are countries ends without ia')
+
+//9
+max = countriesLengths[0]
+let maxIndex = 0
+for(let i=1; i<countriesLengths.length; i++)
+{
+    if(max < countriesLengths[i])
+    {
+        max = countriesLengths[i]
+        maxIndex = i
+    }
+}
+console.log("country containing the biggest number of characters ", countries[maxIndex])
+
+//10
+let fiveCharsCountries = []
+for(let i=0; i<countriesLengths.length; i++)
+{
+    if( countriesLengths[i] == 5)
+    {
+        fiveCharsCountries.push(countries[i])
+    }
+}
+console.log(fiveCharsCountries)
+
+//11
+max = webTechs[0].length
+maxIndex = 0
+for(let i=1; i<webTechs.length; i++)
+{
+    if(max < webTechs[i].length)
+    {
+        max = webTechs[i].length
+        maxIndex = i
+    }
+}
+console.log(webTechs[maxIndex])
+
+//12
+let webTechsInfoArray = []
+for(let i = 0; i < webTechs.length; i++)
+{
+    webTechsInfoArray.push([webTechs[i], webTechs[i].length])
+}
+console.log(webTechsInfoArray)
+
+//13
+let acronym = ""
+for(let mern of mernStack)
+{
+    acronym = acronym.concat(mern.charAt(0))
+}
+console.log(acronym)
+
+//14
+for(let i=0; i<webTechs.length; i++)
+{
+    console.log(webTechs[i])
+}
+
+//15
+let fruits = ['banana', 'orange', 'mango', 'lemon']
+for(let i=0; i < parseInt(fruits.length/2); i++)
+{
+    let tempSwapVar = fruits[i]
+    fruits[i] = fruits[fruits.length-(i+1)]
+    fruits[fruits.length-(i+1)] = tempSwapVar
+}
+console.log(fruits)
+
+//16
+const fullStack = [
+    ['HTML', 'CSS', 'JS', 'React'],
+    ['Node', 'Express', 'MongoDB']
+  ]
+for(let fs of fullStack)
+{
+    for(let webTech of fs)
+    {
+        console.log(webTech)
+    }
+}
