2d61b374bc2402dfec041518d62ae4f15b199d28
Viet Hung Dang <viet.dang@student.manchester.ac.uk>
2023-02-23 17:45:40 +0000

    Day 9 level 3 exercises complete

--- 30daysOfJS/day9/exercises.js
+++ 30daysOfJS/day9/exercises.js
@@ -202,3 +202,180 @@ function mostUsedStartLetter(countries)
     })[0]))[0]
 }
 console.log(mostUsedStartLetter(countries))
+
+//level 3
+
+//1 - sort by name
+const countriesSortedByName = (countries_data) => countries_data.sort( (a, b) => {
+    if(a.name < b.name) return -1
+    else if (a.name > b.name) return 1
+    return 0
+})
+console.log(countriesSortedByName(JSON.parse(JSON.stringify(countries_data))))
+
+//1 - sort by capital
+const countriesSortedByCapital = (countries_data) => countries_data.sort( (a, b) => {
+    if(a.capital < b.capital) return -1
+    else if (a.capital > b.capital) return 1
+    return 0
+})
+console.log(countriesSortedByCapital(JSON.parse(JSON.stringify(countries_data))))
+
+//1 - sort by population
+const countriesSortedByPopulation = (countries_data) => countries_data.sort( (a, b) => {
+    if(a.population < b.population) return 1
+    else if (a.population > b.population) return -1
+    return 0
+})
+console.log(countriesSortedByPopulation(JSON.parse(JSON.stringify(countries_data))))
+
+//2 - 10 most spoken languages
+function mostSpokenLanguages(countries, topN)
+{
+    let countCountriesByLanguage = new Object ()
+    countries.forEach(element => {
+        element.languages.forEach( language => {
+            countCountriesByLanguage[language] = (countCountriesByLanguage[language] || 0) + 1;
+        })
+    })
+
+    let arrayCountriesByLanguage = Object.entries(countCountriesByLanguage).map(([key, value]) => ({ [key] : value}))
+    arrayCountriesByLanguage = arrayCountriesByLanguage.sort((a, b) => {
+        if(Object.values(a)[0] > Object.values(b)[0]) return -1
+        if(Object.values(a)[0] < Object.values(b)[0]) return 1
+        return 0
+    })
+
+    return arrayCountriesByLanguage.filter((element, index, array) => {
+        return index < topN
+    }).map(element => {
+        return ({["country"] : Object.keys(element)[0], ["count"] : element[Object.keys(element)[0]] })
+    })
+}
+console.log(mostSpokenLanguages(countries_data, 3))
+
+//3 - 10 most populated countries
+function mostPopulatedCountries(countries, topN)
+{
+    return countriesSortedByPopulation(countries).filter((element, index, array) => {
+        return index < topN
+    }).map(element => {
+        return ({["country"] : element.name, ["population"] : element.population})
+    })
+}
+console.log(mostPopulatedCountries(countries_data, 10))
+
+//4
+/*
+    console.log('Count:', statistics.count()) // 25
+    console.log('Sum: ', statistics.sum()) // 744
+    console.log('Min: ', statistics.min()) // 24
+    console.log('Max: ', statistics.max()) // 38
+    console.log('Range: ', statistics.range() // 14
+    console.log('Mean: ', statistics.mean()) // 30
+    console.log('Median: ',statistics.median()) // 29
+    console.log('Mode: ', statistics.mode()) // {'mode': 26, 'count': 5}
+    console.log('Variance: ',statistics.var()) // 17.5
+    console.log('Standard Deviation: ', statistics.std()) // 4.2
+    console.log('Variance: ',statistics.var()) // 17.5
+    console.log('Frequency Distribution: ',statistics.freqDist()) 
+        // [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
+*/
+
+let statistics = {
+    ages: [],
+    count: function(){
+        return this.ages.length
+    },
+    sum: function(){
+        return this.ages.reduce((prev, curr) => {
+            return prev + curr
+        }, 0)
+    },
+    min: function(){
+        return this.ages.sort((a,b) => {
+            if(a < b) return -1
+            if(a > b) return 1
+            return 0
+        })[0]
+    },
+    max: function(){
+        return this.ages.sort((a,b) => {
+            if(a < b) return 1
+            if(a > b) return -1
+            return 0
+        })[0]
+    },
+    range: function(){
+        return this.max() - this.min()
+    },
+    mean: function(){
+        return this.ages.reduce((a,b) => {
+            return a + b
+        }, 0)/(this.count())
+    },
+    median: function(){
+        return this.ages.sort((a,b) => {
+            if(a < b) return 1
+            if(a > b) return -1
+            return 0
+        })[parseInt(this.ages.length/2)]
+    },
+    mode: function(){
+        let map = new Object
+        this.ages.map(element => {
+            map[element] = (map[element] || 0) + 1
+        })
+
+        let countOfNumber = Object.entries(map).map(([key, value]) => ({ [key] : value}))
+        return [countOfNumber.sort((a,b) => {
+            if(Object.values(a)[0] < Object.values(b)[0]) return 1
+            if(Object.values(a)[0] > Object.values(b)[0]) return -1
+            return 0
+        })[0]].map(element => {
+            return ({["mode"] : parseInt(Object.keys(element)[0]), ["count"] : Object.values(element)[0]})
+        })[0]
+    },
+    variance: function(){
+        return (this.ages.reduce((acc,curr) => {
+            return acc + (parseInt(curr) - this.mean())**2
+        }, 0))/(this.count() - 1)
+    },
+    std: function(){
+        return Math.sqrt(this.variance())
+    },
+    freqDist: function(){
+        let map = new Object
+        this.ages.map(element => {
+            map[element] = (map[element] || 0) + 1
+        })
+
+        let countOfNumbers = Object.entries(map).map(([key, value]) => ({ [key] : value}))
+        let coeficient = 100.0/countOfNumbers.reduce((acc, curr) => {
+            return acc + Object.values(curr)[0]
+        }, 0)
+        
+        return countOfNumbers.sort((a,b) => {
+            if(Object.values(a)[0] < Object.values(b)[0]) return 1
+            if(Object.values(a)[0] > Object.values(b)[0]) return -1
+            return 0
+        }).map(element => {
+            return [Object.values(element)[0] * coeficient, Object.keys(element)[0]]
+        })
+    }
+}
+
+statistics.ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+console.log(statistics.count())
+console.log(statistics.sum())
+console.log(statistics.min())
+console.log(statistics.max())
+console.log(statistics.range())
+console.log(statistics.mean())
+console.log(statistics.median())
+console.log(statistics.mode())
+console.log(statistics.variance())
+console.log(statistics.std())
+console.log(statistics.freqDist())
+
+
