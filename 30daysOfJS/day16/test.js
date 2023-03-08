ed1d1cca4b1e5f0aec876b5877ccf89678d4518c
Viet Hung Dang <viet.dang@student.manchester.ac.uk>
2023-03-08 11:31:23 +0000

    Day 16 complete

---
+++ 30daysOfJS/day16/test.js
@@ -0,0 +1,25 @@
+const usersText = `{
+    "users":[
+      {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
+    ]
+}`
+    
+const usersObj = JSON.parse(usersText, undefined)
+console.log(usersObj)
